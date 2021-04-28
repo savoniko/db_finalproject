1cb363c3a5a7ee208d556c54d8f4d77109ce7048
Walter Galdamez <galdamez.w@husky.neu.edu>
2021-04-27 20:26:12 -0400

    Deleted unnecessary files. Our App still works

--- src/main/java/com/example/springtemplate/repositories/SectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springtemplate.repositories;
-
-import com.example.springtemplate.models.Course;
-import com.example.springtemplate.models.Section;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SectionRepository
-        extends CrudRepository<Section, Integer> {
-}
